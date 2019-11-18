5975207475ba8f5f093ea7918ec5b86cb5d0b76c
MD. Masbahul <mesbahul.sohan@gmail.com>
2019-11-18 10:00:23 +0600

    9b

--- server.js
+++ server.js
@@ -3,24 +3,44 @@ var express = require('express');
 var app = express();
 var server = http.Server(app);
 var bodyParser = require('body-parser');
-
+var mongoose =  require('mongoose')
 let articles = [];
 
 var db,url = "mongodb+srv://sohanUser:masbahul12345@cluster0-yjzcp.mongodb.net/test?retryWrites=true&w=majority"
 
-mongo.MongoClient.connect(url,
-  
-  {userNewUrlParse:true, useUnifiedTopology:true},
-
-  function(err, clientnode){
-    if(err){
-      console.log('Could not connect to MongoDb')
-    }
-    else{
-      db = client.db('node-cw9')
-    }
-
+mongoose.connect(url,
+  {useNewUrlParser:true, useUnifiedTopology:true})
+  mongoose.connection.on('error', function(){
+    console.log('Could not connect to MongoDB')
   })
+
+var articleSchema = new mongoose.Schema(
+  {
+    title: {
+      type : String,
+      required: "Title is required"
+    },
+    content: {
+      type: String,
+      required: "Content is required"
+    }
+  }
+)
+var Article = mongoose.model('Article', articleSchema)
+
+//mongo.MongoClient.connect(url,
+  
+  //{userNewUrlParse:true, useUnifiedTopology:true},
+
+  //function(err, clientnode){
+    //if(err){
+      //console.log('Could not connect to MongoDb')
+    //}
+    //else{
+    //  db = client.db('node-cw9')
+    //}
+
+  //})
 
 
 var save = function(form_data){
@@ -37,9 +57,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.post('/new_article', function(request, response){
 
-  save(request.body)
-  articles.push(request.body);
-  console.log(articles);
+  //save(request.body)
+  let article = new Article(request.body)
+  article.save(function(err, data){
+    if(err){
+      response.status(400).json({msg: "all fields are required"})
+    }
+    response.status(200).json({article:data})
+  })
+  //articles.push(request.body);
+  //console.log(articles);
   response.json({msg: "Successfully recieved"});
 })
 
